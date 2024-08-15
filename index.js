a776be834fa9b03e8ac9063a415fe70509582af9
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-08-15 17:03:22 +0200

    define post request for new order and get request to fetch all orders

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Models = require("./models");
 
-const Order = Models.Order;
+const Order = require("./models");
 
-const uuid = require("uuid");
+// const uuid = require("uuid");
 
 const app = express();
 
@@ -24,22 +24,34 @@ mongoose.connect("mongodb://localhost:27017/orders", {
  */
 app.use(bodyParser.json());
 
-let orders = [];
+// get all orders
+app.get("/orders", async (req, res) => {
+  await Order.find()
+    .then((orders) => {
+      res.status(201).json(orders);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
 // add new order
 app.post("/orders", (req, res) => {
-  let newOrder = req.body;
-
-  //check if "Item" is present in the requested body
-  if (!newOrder.Item) {
-    const message = 'Missing "Item" in request body';
-    res.status(400).send(message);
-  } else {
-    //add unique ID to the order
-    newOrder.id = uuid.v4();
-    orders.push(newOrder);
-    res.status(201).send(newOrder);
-  }
+  Order.create({
+    Item: req.body.Item,
+    Payment: req.body.Payment,
+    Total: req.body.Total,
+    Receiver: req.body.Receiver,
+  })
+    .then((order) => {
+      // console.log("Order created: ", order);
+      res.status(201).json(order);
+    })
+    .catch((error) => {
+      console.log("Error: ", error);
+      // res.status(500).json("Error: " + error);
+    });
 });
 
 // listen for requests
